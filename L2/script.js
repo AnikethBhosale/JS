4ca8ac3e34a844231d136286ee6c9ce03821a4cf
Aniketh <anikethbhosale11@gmail.com>
2025-04-22 20:03:27 +0530

    L2

--- L2/script.js
+++ L2/script.js
@@ -9,9 +9,9 @@
 
 //Arithmetic operator
 
-let a = 5 ;
-let b = 2 ;
-console.log('a=',a,' b=',b);
+// let a = 5 ;
+// let b = 2 ;
+// console.log('a=',a,' b=',b);
 // console.log(' a+b = ',a+b);
 // console.log(' a-b = ',a-b);
 // console.log(' a*b = ',a*b);
@@ -54,3 +54,98 @@ console.log('a=',a,' b=',b);
 // Logical Operators
 
 // && -> And , || -> OR , !-> NOT
+
+
+
+
+// CONDITIONAL STATEMENTS
+
+// if and if-else statements & else-if
+
+//let age = 16;
+// let mode = 'blue';
+// let color;
+// if (mode==='dark')
+// {
+//   color = 'black';
+// }
+// else
+// {
+//   color = 'white';
+// }
+
+// console.log(color);
+// if (age>=18)
+// {
+//   console.log('Can Vote');
+// }
+// else
+// {
+//   console.log('Cannot vote');
+// }
+
+// let age = 20;
+// if (age < 18 ) {
+//   console.log('junior');      
+//   }
+// else if (age > 60){
+//   console.log('senior');
+// }
+// else {
+//   console.log('middle');
+// }
+
+
+
+
+//Ternary operator 
+
+//a? b:c
+
+//let age =35;
+
+// let result = age >= 18 ? 'adult' : 'not adult';
+// console.log(result);
+
+//age >= 18 ? console.log('adult') : console.log('not adult'); // simpler , compact if-else
+
+
+
+
+
+
+
+
+
+/// PRACTICE 
+// Q1.
+
+// let num = prompt('Enter an number:');
+// let res = num%5==0 ? 'is multiple' : 'not a multiple';
+// console.log(num , res);
+
+
+// Q2.
+
+// let scores = prompt('enter score');
+
+// if (scores>= 90 && scores <=100)
+// {
+//   console.log('Grade A');
+// }
+// else if (scores>= 70 && scores <=89)
+// {
+//   console.log('Grade B');
+// }
+// else if (scores>= 60 && scores <=69)
+// {
+//   console.log('Grade C');
+// }
+// else if (scores>= 50 && scores <=59)
+// {
+//   console.log('Grade D');
+// }
+// else if (scores<=49 && scores >= 0)
+// {
+//   console.log('Grade F');
+// }  
