d57a2055699c2e9f3809524be4818ef2ab27e2ee
Aniketh <anikethbhosale11@gmail.com>
2025-04-22 18:23:14 +0530

    .

---
+++ L2/script.js
@@ -0,0 +1,56 @@
+//single line Comment
+
+//console.log('Hello world!')
+
+/* multi-line 
+ comment*/
+
+
+
+//Arithmetic operator
+
+let a = 5 ;
+let b = 2 ;
+console.log('a=',a,' b=',b);
+// console.log(' a+b = ',a+b);
+// console.log(' a-b = ',a-b);
+// console.log(' a*b = ',a*b);
+// console.log(' a/b = ',a/b);
+// console.log(' a%b = ',a%b);
+// console.log(' a**b = ',a**b);
+// console.log(' ++a = ',++a);
+// console.log(' --a = ',--a);
+// console.log(' a++ = ',a++);
+// console.log(' a-- = ',a--);
+// console.log('a=',a);
+
+// (crtl + /) --> commenting multiple lines at once
+
+
+// Assignment Operators 
+/*
+1. = 
+2. +=
+3. -=
+4. *=
+5. /=
+6. %=
+7. **=
+*/
+
+// Comparison Operator
+
+// console.log('a==b',a==b);
+// console.log('a==a',a=='5');
+// console.log('a!=b',a!=b);
+// console.log('a===b',a===b);
+// console.log('a!==b',a!==b);
+// console.log('a===a',a==='5');
+// console.log('a!==a',a!=='5');
+// > , >= , < , <= 
+
+
+
+// Logical Operators
+
+// && -> And , || -> OR , !-> NOT
