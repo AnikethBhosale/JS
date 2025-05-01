6547610504af17d80fcbdb35bfb86581e49839da
Aniketh <anikethbhosale11@gmail.com>
2025-05-01 22:36:06 +0530

    .

---
+++ L3/script.js
@@ -0,0 +1,37 @@
+//console.log('Aniketh')
+
+// for loop 1 to 5
+//     initialize    stoping cond     updating
+// for (let i=1 ;          i<=5 ;          i++)
+// {
+//   console.log('Aniketh');
+// }
+
+
+
+// sum of 1 to 5
+
+// let sum = 0;
+// for(let i=1; i<=5;i++){
+//    sum += i;
+// }
+
+//console.log(i); // i is block scope variable
+// but is we had used "var" it would have been a global variable and we could have accessed it
+// console.log('sum is: ',sum);
+// console.log('Loop has ended');
+
+
+//infintie loop /// DO NOT RUN THIS CODE
+// for (let i=1 ;   i<=0 ; i++)
+//   {
+//     console.log('Aniketh');
+//   }
+  
+
+
+let  i = 1 ;
+while(i<=5){
+  console.log(i);
+  i++;
+}
