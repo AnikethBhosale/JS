a3eee7de3ed951fbeb0a725472933f73b7f6142f
Aniketh <anikethbhosale11@gmail.com>
2025-05-25 15:28:43 +0530

    .

---
+++ L5/fifth.js
@@ -0,0 +1,38 @@
+// function myFunction() {
+//   console.log('Hello!!!');
+//   console.log('We are learning JS');
+// }
+
+
+// myFunction();
+// myFunction();
+
+
+
+// function myFunction(msg) {
+//   // parameter --> input
+//   console.log(msg);
+// }
+
+// myFunction('Hello'); // argument --> input , but at function call 
+
+
+
+// Function -> 2 numbers, sum
+
+// function sum(x,y){
+//   console.log(x+y);
+// }
+
+// sum(5,4);
+
+
+// return the sum , don't print on console
+
+// function sum(x,y){
+//   s = x+y;
+//   return s ;
+// }
+
+// let val = sum(5,4);
+// console.log(val);
