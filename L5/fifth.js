833602eca11bec5306b69cbe4fe524ed07ab8bc4
Aniketh <anikethbhosale11@gmail.com>
2025-06-22 13:10:39 +0530

    .

--- L5/fifth.js
+++ L5/fifth.js
@@ -36,3 +36,68 @@
 
 // let val = sum(5,4);
 // console.log(val);
+
+
+
+
+
+//// ARROW FUNCTIONS //////////////
+
+//Normal Functions
+
+// function sum(a,b){
+//   return a+b;
+// }
+
+// function mul(a,b){
+//   return a*b;
+// }
+
+
+/// arrow function 
+// const arrowSum = (a,b) => {
+//   console.log(a+b);
+// }
+
+// const arrowMul = (a,b) => {
+//   return (a*b);
+// }
+
+// const printHello = () => {
+//   console.log("Hello");
+// }
+
+
+
+
+///////// Let's Practice ///////
+
+// Q: Create a function using the "function" keyword that takes a String as an argument & return the number of vowels in the string
+
+// function countVowels(str) {
+//   let count = 0;
+
+//   for(const char of str){
+    // if(char==="a" || char==="e" || char==="i" || char==="o" || char=="u")
+    // {
+    //   count++;
+    // }
+//   }
+//   return count;
+// }
+
+
+// Q: Create an arrow function to perform the same
+
+// const countVowels = (str) => {
+//   let count = 0;
+
+//   for(const char of str)
+//   {
+//     if(char==="a" || char==="e" || char==="i" || char==="o" || char=="u")
+//     {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
