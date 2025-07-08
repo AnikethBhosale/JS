20fafdd9cebf03393330a9ebe1af1e662cbd1391
Aniketh <anikethbhosale11@gmail.com>
2025-07-08 21:57:31 +0530

    .

--- L5/fifth.js
+++ L5/fifth.js
@@ -101,3 +101,149 @@
 //   }
 //   return count;
 // }
+
+
+/// ForEach Loop
+
+// let arr = [1,2,3,4,5] ; 
+
+// arr.forEach(function printVal(val){ 
+    // forEach will pass each value at each index to printVal as arguments and we need not execute the inner function , forEach automatically executes it.
+//     console.log(val);
+// })
+
+// let arr = [1,2,3,4,5];
+
+// arr.forEach((val) => {
+//     console.log(val);
+// })
+
+// let arr = ["Bengaluru" , "Delhi" , "Pune"];
+// for a callBack function we can make use of three parameters - 
+// values (items) , index (position) , array itself
+// arr.forEach((val,idx,arr) => {
+//     console.log(val,idx,arr);
+// })
+
+
+
+
+
+// Let's Practice 
+// Q1. For a given array of numbers , print the square of each value using the forEach loop.
+
+// let arr = [1,2,3,4,5] ;
+
+// arr.forEach((val)=>{
+//     console.log("Square of "+val +" is "+val*val); // or val**2
+// });
+
+
+// other method
+
+// let arr = [1,2,3,4,5] ;
+
+// let calcSquare = (val) => {
+//     console.log("Square of "+val +" is "+val*val); // or val**2
+// }
+
+// arr.forEach(calcSquare);
+
+
+
+// Some more array methods. 
+
+
+
+// map --> returns new array after some operation
+
+// let nums = [67, 52, 39]
+
+// nums.map((val) => {
+//     console.log(val);
+// });
+
+//copy of array
+// let newArray = nums.map((val) => {
+//     return val;
+// });
+
+//double the values in nums and make new array
+// let newArray = nums.map((val) => {
+//     return val*2;
+// });
+// console.log(newArray);
+
+
+
+// Filter method :
+// let nums = [67, 52, 39, 48]
+
+// let evenArray = nums.filter((val)=>{
+//     return val%2===0;
+// });
+// console.log(evenArray);
+
+// let greaterThsn40 = nums.filter((val)=>{
+//     return val>40;
+// });
+// console.log(greaterThsn40);
+
+
+//Reduce method :
+// let arr = [1, 2, 3, 4]
+// accumulator & currentVAl are previous value and current value
+// const output = arr.reduce((res,curr)=>{
+//     return res+curr;
+// })
+// console.log(output);
+
+//max val
+// arr = [11, 22, 34, 14]
+// let maxVal = arr.reduce((prev,curr)=>{
+//     return prev>curr ? prev : curr;
+// })
+// console.log(maxVal);
+
+//avg val
+// arr = [10, 20, 30, 20]
+// let avgVal = arr.reduce((prev,curr)=>prev+curr )  / arr.length;
+// console.log(avgVal);
+
+
+
+
+// Lets Practice 
+// Q. We are given an array of marks of students. Filter out the marks of the students that scored 90+ . 
+
+// let marks = [90, 87, 98, 69, 99, 88, 98, 95];
+// let toppers = marks.filter((val)=>{
+//     return val > 90;
+// })
+// console.log(toppers);
+
+
+
+
+//Let's Practice 
+// Q. Take a number n as input from user. Create an array of numbers from 1 to n. 
+// Use reduce to find the sum of all numbers.
+// Use reduce to find product of all numbers in the array.
+
+// let n = prompt("Enter a number");
+// let nums = [];
+// let i;
+// for(i=1;i<=n;i++){
+//     nums.push(i);
+// }
+
+// let sumOfNums = nums.reduce((prev,curr)=>{
+//     return prev+curr;
+// })
+
+// let factorial = nums.reduce((prev,curr)=>{
+//     return prev*curr;
+// })
+
+// console.log(sumOfNums);
+// console.log(factorial);
